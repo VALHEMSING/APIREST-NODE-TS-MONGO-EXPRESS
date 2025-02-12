287eba8fc12b9b041f74cc057a5ec743a434d13b
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-02-11 21:37:46 -0500

    feat(backend-controller): Agregue un nuevo controlador para user, findAllUsersController, con este recibe y por el servicio se encargara de realizar lo interno

--- APIREST-NODE-TS-MONGO/src/controllers/user.controllers.ts
+++ APIREST-NODE-TS-MONGO/src/controllers/user.controllers.ts
@@ -1,4 +1,4 @@
-import { createUserService } from "@services/user.services";
+import { createUserService, findAllUsersService } from "@services/user.services";
 import { Request, Response } from "express";
 
 
@@ -11,5 +11,16 @@ export const createUserController = async (req: Request, res: Response) => {
     } catch (error) {
         console.error(`Error en createUserController: ${error}`);
         res.status(500).json({message: "Controller: Error al crear el usuario"});
+    }
+}
+
+// Controlador para obtener todos los usuarios
+export const findAllUsersController = async (req: Request, res: Response) => {
+    try {
+        const users = await findAllUsersService();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(`Error en findAllUsersController: ${error}`);
+        res.status(500).json({message: "Controller: Error al obtener los usuarios"});
     }
 }
