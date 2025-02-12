4ce0b13d821995d7e5f4cc9dfd2e5db1f9c25efe
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-02-11 21:50:25 -0500

    feat(backend-routes): Agregue las rutas para los controladores agregados, ya fueron probados y funcionan correctamente...

--- APIREST-NODE-TS-MONGO/src/routes/user.routes.ts
+++ APIREST-NODE-TS-MONGO/src/routes/user.routes.ts
@@ -1,4 +1,10 @@
-import { createUserController } from "@controllers/user.controllers";
+import { 
+    createUserController,
+    deleteUserByIdController, 
+    findAllUsersController, 
+    findUserByIdController, 
+    updateUserByIdController 
+} from "@controllers/user.controllers";
 import { Router } from "express";
 
 
@@ -8,6 +14,14 @@ const router = Router();
 
 // Ruta para crear un usuario
 router.post('/', createUserController)
+// Ruta para obtener todos los usuarios
+router.get('/', findAllUsersController)
+// Ruta para obtener un usuario por su id
+ router.get('/:id', findUserByIdController)
+// Ruta para actualizar un usuario por su id
+router.put('/:id', updateUserByIdController)
+// Ruta para eliminar un usuario por su id
+router.delete('/:id', deleteUserByIdController)
 
 
 
