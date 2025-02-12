2d51368351f9c324f86081f18c5fa73960809a21
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-02-11 21:55:10 -0500

    feat(backend-controller): Modifique para las respuestas que tuvieran un mensaje, al realizarce de manera correcta la solicitud

--- APIREST-NODE-TS-MONGO/src/controllers/user.controllers.ts
+++ APIREST-NODE-TS-MONGO/src/controllers/user.controllers.ts
@@ -7,7 +7,7 @@ import { Request, Response } from "express";
 export const createUserController = async (req: Request, res: Response) => {
     try {
         const user = await createUserService(req.body);
-        res.status(201).json(user);
+        res.status(201).json({message: "User created", user:user});
     } catch (error) {
         console.error(`Error en createUserController: ${error}`);
         res.status(500).json({message: "Controller: Error al crear el usuario"});
@@ -30,7 +30,7 @@ export const findUserByIdController = async (req: Request, res: Response) => {
     try {
         const { id } = req.params
         const user = await findUserByIdService(id);
-        res.status(200).json(user);
+        res.status(200).json({message: "User found", user: user});
     } catch (error) {
         console.error(`Error en findUserByIdController: ${error}`);
         res.status(500).json({message: "Controller: Error al obtener el usuario"});
@@ -42,7 +42,7 @@ export const updateUserByIdController = async (req: Request, res: Response) => {
     try {
         const { id } = req.params
         const user = await updateUserByIdService(id, req.body);
-        res.status(200).json(user);
+        res.status(200).json({message: "User updated", user: user});
     } catch (error) {
         console.error(`Error en updateUserByIdController: ${error}`);
         res.status(500).json({message: "Controller: Error al actualizar el usuario"});
@@ -55,7 +55,7 @@ export const deleteUserByIdController = async (req: Request, res: Response) => {
     try {
         const { id } = req.params
         const user = await deleteUserByIdService(id);
-        res.status(200).json(user);
+        res.status(200).json({message: "User deleted", user:user});
     } catch (error) {
         console.error(`Error en deleteUserByIdController: ${error}`);
         res.status(500).json({message: "Controller: Error al eliminar el usuario"});
